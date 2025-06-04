bd1c5a3c613c99ee74db666943d5250b4532729b
niceduckdev <bottykaj@gmail.com>
2025-06-04 21:18:47 +0200

    route change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
-    },
-    redirects: {
-        "/": "/about-me",
-    },
+    }
 });
